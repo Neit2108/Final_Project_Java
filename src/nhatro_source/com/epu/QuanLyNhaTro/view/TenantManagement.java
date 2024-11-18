544ec16c5d13d009316e5e2159191af039c6dbf0
Neit2108 <tienld.22810310329@epu.edu.vn>
2024-11-18 22:16:46 +0700

    menucontroller

--- src/nhatro_source/com/epu/QuanLyNhaTro/view/TenantManagement.java
+++ src/nhatro_source/com/epu/QuanLyNhaTro/view/TenantManagement.java
@@ -1,10 +1,12 @@
 package com.epu.QuanLyNhaTro.view;
 
+import com.epu.QuanLyNhaTro.controller.TenantManagerController;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 
 @Getter
@@ -27,9 +29,12 @@ public class TenantManagement extends JPanel {
     private JButton searchBtn;
     private JTable mainTable;
     private JPanel mainTablePanel;
+    private JTextField accountNumberField;
+    private DefaultTableModel tableModel;
 
     public TenantManagement() {
         initComponents();
+        new TenantManagerController(this).init();
     }
 
     private void initComponents() {
@@ -59,7 +64,7 @@ public class TenantManagement extends JPanel {
 
     private JPanel createInputPanel() {
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(7, 2, 10, 10));
+        panel.setLayout(new GridLayout(8, 2, 10, 10));
         panel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),
                 "Nhập thông tin khách thuê", TitledBorder.CENTER, TitledBorder.TOP));
 
@@ -70,6 +75,7 @@ public class TenantManagement extends JPanel {
         genderField = new JTextField(20);
         phoneField = new JTextField(20);
         addressField = new JTextField(20);
+        accountNumberField = new JTextField(20);
 
         panel.add(new JLabel("Mã khách:"));
         panel.add(numberField);
@@ -91,6 +97,9 @@ public class TenantManagement extends JPanel {
 
         panel.add(new JLabel("Địa chỉ:"));
         panel.add(addressField);
+
+        panel.add(new JLabel("Mã tài khoản:"));
+        panel.add(accountNumberField);
 
         return panel;
     }
@@ -128,9 +137,17 @@ public class TenantManagement extends JPanel {
 
         // Table
         String[] columnNames = {"Mã khách", "CCCD", "Tên khách", "Ngày sinh", "Giới tính", "Số điện thoại", "Địa chỉ", "Mã tài khoản"};
-        mainTable = new JTable(new Object[][]{}, columnNames);
+        this.tableModel = new DefaultTableModel(columnNames, 0){
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
+
+        mainTable = new JTable(tableModel);
         mainTable.setRowHeight(30);
         mainTable.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+        mainTable.getTableHeader().setReorderingAllowed(false);
 
         JScrollPane scrollPane = new JScrollPane(mainTable);
         panel.add(scrollPane, BorderLayout.CENTER);
