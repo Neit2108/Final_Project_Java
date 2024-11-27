00a55f8b19a55eb652c75046d6de490a1119c4cf
Neit2108 <tienld.22810310329@epu.edu.vn>
2024-11-27 18:49:23 +0700

    .

--- src/nhatro_source/com/epu/QuanLyNhaTro/util/Constant.java
+++ src/nhatro_source/com/epu/QuanLyNhaTro/util/Constant.java
@@ -2,14 +2,15 @@ package com.epu.QuanLyNhaTro.util;
 
 public class Constant {
     //Database
-    public final static String url = "jdbc:sqlserver://localhost:1433;databaseName=final_QuanLyNhaTro_db;encrypt=true;trustServerCertificate=true";
+    public final static String url = "jdbc:sqlserver://localhost:1433;databaseName=final_QuanLyNhaTro;encrypt=true;trustServerCertificate=true";
     public final static String username = "sa";
-    public final static String password = "ntq14102004";
+    public final static String password = "23102001";
 
     //Mail
-    public final static String senderMail = "ldungtien.210804@gmail.com";
+    public final static String senderMail = "";
     public final static String senderPass = "cncawmbjswiyagwf";
 
-    public static String role;
+    //Role
+    public static String role = "Admin";
 
 }
